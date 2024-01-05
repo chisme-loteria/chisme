96d338ac85115e297035ac404c15a22ac572e157
Your Mom <chisme.loteria@gmail.com>
2024-01-05 06:05:07 -0600

    plssssss

--- svelte.config.js
+++ svelte.config.js
@@ -5,16 +5,16 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter(),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
+	}
 };
 
 export default config;
